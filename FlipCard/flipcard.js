dd209745be0d7d54a8bc2e2f0efcaaff1127631f
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-06 20:01:57 +0000

    Created animation to flip card as if playing from deck and movign to discard pile

--- FlipCard/flipcard.js
+++ FlipCard/flipcard.js
@@ -1,7 +1,12 @@
 
 var card = document.querySelector('.card');
 card.addEventListener('click', function () {
-  card.classList.toggle('is-flipped');
+  if (card.classList.contains('is-flipped')) {
+    card.classList.remove('is-flipped');
+    card.classList.add('recycled');
+  } else {
+    card.classList.toggle('is-flipped');
+  }
 });
 
 // toggle is like if (true) classList.remove() else if (!true) classList.add()
