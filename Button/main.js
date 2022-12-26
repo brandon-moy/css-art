c379972e0fc72d5919397c4ccbb846b933453e31
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-26 20:02:42 +0000

    Created function to cycle 'levels' and added different colors for background

--- Button/main.js
+++ Button/main.js
@@ -1,6 +1,13 @@
-const $troll = document.querySelector('.follow-me');
+const $button = document.querySelector('.follow-me');
 const $body = document.querySelector('body');
 
-$troll.addEventListener('click', () => {
-  $body.style.background = 'blue';
+let lastLevel = 0;
+let currentLevel = 1;
+
+$button.addEventListener('click', () => {
+  lastLevel = currentLevel;
+  currentLevel++;
+  $body.className = 'body' + currentLevel;
+  $button.classList.remove('button' + lastLevel);
+  $button.classList.add('button' + currentLevel);
 });
