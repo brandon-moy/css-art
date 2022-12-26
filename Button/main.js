d0717f667e28e74b6fc0658484fe9b4811c7270a
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-26 20:19:33 +0000

    Pseudocoded plan for making button randomize page background and button location

--- Button/main.js
+++ Button/main.js
@@ -11,3 +11,29 @@ $button.addEventListener('click', () => {
   $button.classList.remove('button' + lastLevel);
   $button.classList.add('button' + currentLevel);
 });
+
+// What should click event do?
+
+// Set last level to the current level
+// Increment current level by 1
+
+// Randomize background and randomize button location
+
+// for background:
+// Option A: create background color variables
+// create an array containing the variables
+// randomly pull colors from the array and set them as a linear gradient
+// create an array containing directions
+// randomly pull a direction for the gradient to move towards
+
+// Option B: use Math.random to give a number;
+// multiple that number by 255
+// take the floor of that number
+// do this 3 times to give us an rgb value
+
+// Then after whichever option, use the style property to update the body element
+
+// for Button
+// take max width and height of screen?
+// then take Math.random and multiple by the width and height of the screen
+// move the button to that position in pixels?
