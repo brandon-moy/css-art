7c0978633e2c435fd7e0b39cbeea7df55466f477
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-12-27 19:58:22 +0000

    Removed CSS variable colors and commented out code

--- Button/main.js
+++ Button/main.js
@@ -8,15 +8,6 @@ $button.addEventListener('click', () => {
   handleButton();
 });
 
-// console.log(window.innerWidth);
-// console.log(window.innerHeight);
-
-// What should click event do?
-// Set last level to the current level
-// Increment current level by 1
-
-// Randomize background and randomize button location
-
 function handleBackground(event) {
   const color1 = createBackgroundColor();
   const color2 = createBackgroundColor();
@@ -33,24 +24,6 @@ function createBackgroundColor() {
   const b = Math.floor(Math.random() * 255);
   return `rgb(${r}, ${g}, ${b})`;
 }
-// for background:
-// Option A: create background color variables
-// create an array containing the variables
-// randomly pull colors from the array and set them as a linear gradient
-// create an array containing directions
-// randomly pull a direction for the gradient to move towards
-
-// Option B: use Math.random to give a number;
-// multiple that number by 255
-// take the floor of that number
-// do this 3 times to give us an rgb value
-
-// Then after whichever option, use the style property to update the body element
-
-// for Button
-// take max width and height of screen?
-// then take Math.random and multiple by the width and height of the screen
-// move the button to that position in pixels?
 
 function handleButton(event) {
   const positionX = Math.floor(Math.random() * window.innerWidth);
