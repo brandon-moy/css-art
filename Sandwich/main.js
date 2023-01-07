29f3724fc97012275bc93e855ffbec12a6ba7ef8
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-07 18:10:06 +0000

    Created function to move pieces of bread around page based on mousemove

---
+++ Sandwich/main.js
@@ -0,0 +1,20 @@
+let target = null;
+
+window.addEventListener('mousedown', mouseDown);
+window.addEventListener('mouseup', mouseUp);
+window.addEventListener('mousemove', move);
+
+function mouseUp() {
+  target = null;
+}
+
+function mouseDown(e) {
+  if (e.target.tagName !== 'path') return;
+  target = e.target.closest('svg');
+}
+
+function move(e) {
+  if (!target) return;
+  target.style.top = e.clientY + 'px';
+  target.style.left = e.clientX + 'px';
+}
