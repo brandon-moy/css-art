700521d606a799fce16ed53b00d6656138fdb95a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-18 21:51:37 +0000

    Added condition to check environment to pass through node

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-const track = document.querySelector('.track');
+if (typeof window === 'object') {
 
-const handleOnDown = e => {
-  track.dataset.mouseDownAt = e.clientX;
-};
+  const track = document.querySelector('.track');
 
-const handleOnUp = () => {
-  track.dataset.mouseDownAt = '0';
-  track.dataset.prevPercentage = track.dataset.percentage;
-};
+  const handleOnDown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+  };
 
-const handleOnMove = e => {
-  if (track.dataset.mouseDownAt === '0') return;
+  const handleOnUp = () => {
+    track.dataset.mouseDownAt = '0';
+    track.dataset.prevPercentage = track.dataset.percentage;
+  };
 
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
-  const maxDelta = window.innerWidth / 2;
+  const handleOnMove = e => {
+    if (track.dataset.mouseDownAt === '0') return;
 
-  const percentage = (mouseDelta / maxDelta) * -100;
-  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
-  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    const maxDelta = window.innerWidth / 2;
 
-  track.dataset.percentage = nextPercentage;
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
-  track.animate({
-    transform: `translateX(${nextPercentage}%)`
-  }, { duration: 1200, fill: 'forwards' });
-};
+    track.dataset.percentage = nextPercentage;
 
-window.onmousedown = e => handleOnDown(e);
+    track.animate({
+      transform: `translateX(${nextPercentage}%)`
+    }, { duration: 1200, fill: 'forwards' });
+  };
 
-window.ontouchstart = e => handleOnDown(e.touches[0]);
+  window.onmousedown = e => handleOnDown(e);
 
-window.onmouseup = e => handleOnUp(e);
+  window.ontouchstart = e => handleOnDown(e.touches[0]);
 
-window.ontouchend = e => handleOnUp(e.touches[0]);
+  window.onmouseup = e => handleOnUp(e);
 
-window.onmousemove = e => handleOnMove(e);
+  window.ontouchend = e => handleOnUp(e.touches[0]);
 
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+  window.onmousemove = e => handleOnMove(e);
+
+  window.ontouchmove = e => handleOnMove(e.touches[0]);
+}
