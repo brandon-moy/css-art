0c503edb240c3292de7638a2b9e4726c0914b90a
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-01-18 19:27:32 +0000

    Deleted code for greeting spill and renamed yeet function and animations to spill

--- Fries/main.js
+++ Fries/main.js
@@ -2,12 +2,12 @@ const scene = document.querySelector('.scene');
 const fries = document.querySelectorAll('.fry');
 const shadow = document.querySelector('.shadow');
 
-scene.addEventListener('click', yeet);
+scene.addEventListener('click', spill);
 
-function yeet(event) {
-  scene.style.animation = 'yeet 5s ease-in-out 1';
+function spill(event) {
+  scene.style.animation = 'spill 5s ease-in-out 1';
   scene.style.animationFillMode = 'forwards';
-  shadow.style.animation = 'yeet-shadow 5s ease-in-out 1';
+  shadow.style.animation = 'spill-shadow 5s ease-in-out 1';
   shadow.style.animationFillMode = 'forwards';
   for (let i = 0; i < fries.length; i++) {
     fries[i].style.animation = `fall${i + 1} 5s ease-in-out 1`;
