7581ca7914670ca857a0dd94b9b4bb8fd295fae5
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-04 19:55:34 +0000

    Modified interval to also include rotation of pentagon and color change

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const $sets = document.querySelectorAll('.set');
+const $pentagon = document.querySelector('.pentagon');
 
-let position = 0;
+let position = 1;
+const colors = ['var(--yellow)', 'var(--purple)', 'var(--pink)'];
+let rotations = -15;
 
 setInterval(() => {
   if (position === 3) {
@@ -13,5 +16,8 @@ setInterval(() => {
       $sets[i].className = 'set';
     }
   }
+  rotations = rotations + 72;
+  $pentagon.style.fill = colors[position];
+  $pentagon.style.transform = `scale(1.5) rotate(${rotations}deg)`;
   position++;
 }, 5000);
