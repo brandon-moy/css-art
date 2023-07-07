2bed74f7039bc89275a0c1db1a4cad47e68da1ba
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-07-07 17:52:59 +0000

    Positioned circle on arc with Math PI and sin/cos

--- polyrhythmn/main.js
+++ polyrhythmn/main.js
@@ -10,15 +10,12 @@ const draw = () => {
     y: paper.height * 0.9
   };
 
-  // const center = {
-  //   x: paper.width * 0.5,
-  //   y: paper.height * 0.9
-  // };
-
   const end = {
     x: paper.width * 0.9,
-    y: paper.height * 0.9
+    y: paper
   };
+
+  const length = end.x - start.x;
 
   pen.strokeStyle = 'white';
   pen.lineWidth = 6;
@@ -28,9 +25,28 @@ const draw = () => {
   pen.lineTo(end.x, end.y);
   pen.stroke();
 
+  const center = {
+    x: paper.width * 0.5,
+    y: paper.height * 0.9
+  };
+
+  // draw arc
   pen.beginPath();
-  pen.arc();
+  pen.arc(center.x, center.y, length * 0.05, Math.PI, 2 * Math.PI);
   pen.stroke();
+
+  const arcRadius = length * 0.05;
+  const angle = Math.PI;
+
+  const x = center.x + arcRadius * Math.cos(angle);
+  const y = center.y + arcRadius * Math.sin(angle);
+
+  // draw circle
+  pen.fillStyle = 'white';
+  pen.beginPath();
+  pen.arc(x, y, length * 0.01, 0, 2 * Math.PI);
+  pen.fill();
+
 };
 
 draw();
